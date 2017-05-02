829d877acbe487ea380942be7f4f6ddde157472b
cspenc <crs992@gmail.com>
2017-05-02 09:13:18 -0400

    get vals from html to add to db

--- app.js
+++ app.js
@@ -12,5 +12,15 @@ function showResults(data) {
   $('.title').html(` ${data.Title}`);
   $('.mpn').html(` ${data.MPN}`);
   $('.price').html(` ${data.Price}`);
-  $('.add').html(`<button class="btn btn-success" onClick="searchAmazon('B00W2KG92Y')">Add to Database</button>`);
+  $('.add').html(`<button class="btn btn-success" onClick="addToDB()">Add to Database</button>`);
+}
+
+function addToDB() {
+  var asin = $('.asin').html();
+  var title = $('.title').html();
+  var mpn = $('.mpn').html();
+  var price = $('.price').html();
+
+  //this is where the stuff gets added to the DB
+
 }
